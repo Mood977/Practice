9d30e3f9fee02e013913e8b51bb1838ab7e24b6d
Mahmoud Kandeel <mood977@yahoo.com>
2025-05-01 15:44:53 +0300

    Test import libraries

--- function_test.py
+++ function_test.py
@@ -1,6 +1,14 @@
+import time
+import requests
 
 def add(num1, num2):
     return num1 + num2
 
 print(add(4, 8))
 
+for number in range(1,3):
+    print(number)
+    time.sleep(1)
+
+ip = requests.get("https://api.ipify.org").text
+print(f"My public IP address is: {ip}")
