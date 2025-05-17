37dd5ac0f4d32cc9a04b070f5145021ab73fcedb
Mahmoud Kandeel <mood977@yahoo.com>
2025-05-17 14:12:06 +0300

    refine the edit interface script for IOS-XE device

--- Devices/IOS-XE/NETCONF/netconf_edit_int.py
+++ Devices/IOS-XE/NETCONF/netconf_edit_int.py
@@ -1,23 +1,23 @@
 import sys
 sys.path.append(r"d:\Practice\Practice\Devices")
 from ncclient import manager
-from device_info import ios_xr_device
-from connect_functions import connect_ios_xr
+from device_info import ios_xe_device
+from connect_functions import connect_ios_xe
 
 
-edit_tepmplate = open(r"d:\Practice\Practice\Devices\IOS-XE\NETCONF\config-temp-interfaces.xml").read()
+edit_template = open(r"d:\Practice\Practice\Devices\IOS-XE\NETCONF\config-temp-interfaces.xml").read()
 
-netconf_payload = edit_tepmplate.format(int_name="Loopback999",
+netconf_payload = edit_template.format(int_name="Loopback1001",
                                         int_desc="MSK Loopback Created using NETCONF",
                                         ip_address="20.20.20.30",
-                                        prefix_length="255.255.255.255")
+                                        prefix_length="32")
 
 
 print("Conf Payload:")
 print("====================================")
 print(netconf_payload)
 
-with connect_ios_xr(ios_xr_device) as m: 
+with connect_ios_xe(ios_xe_device) as m: 
     # Send NETCONF <edit-config>
     netconf_reply = m.edit_config(netconf_payload, target="running")
     print(netconf_reply)
